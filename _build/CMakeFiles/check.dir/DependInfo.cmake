162be51e8561ac200e3127b531d42cae37104804
Evgengrmit <evgengrmit@gmail.com>
2020-04-20 21:31:13 +0300

    Проверка

---
+++ _build/CMakeFiles/check.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/tests/test1.cpp" "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/CMakeFiles/check.dir/tests/test1.cpp.o"
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/tests/test2.cpp" "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/CMakeFiles/check.dir/tests/test2.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../banking"
+  "../third-party/gtest/googletest/include"
+  "../third-party/gtest/googletest"
+  "../third-party/gtest/googlemock/include"
+  "../third-party/gtest/googlemock"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/CMakeFiles/account.dir/DependInfo.cmake"
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/CMakeFiles/transaction.dir/DependInfo.cmake"
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/third-party/gtest/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/third-party/gtest/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake"
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/third-party/gtest/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/third-party/gtest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/evgengrmit/Evgengrmit/workspace/projects/hw05
+# Build directory: /Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/_build/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/CMakeLists.txt;24;add_test;/Users/evgengrmit/Evgengrmit/workspace/projects/hw05/CMakeLists.txt;0;")
+subdirs("third-party/gtest")
